ce2ba02ccdb378be6111d20be452817431c68eb7
Asep Muhidin <asep.muhidin@gmail.com>
2023-01-23 11:36:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,6 @@ if case_folding:
      Case Folding adalah proses perubahan huruf kapital menjadi huruf kecil. 
      Tujuannya yaitu kata-kata yang sama tidak terdeteksi berbeda hanya karena perbedaan terdapat huruf kapital.""")
          
-     txt_cf=st.text_area("""**Masukkan sembarang teks/paragraph**""",value='''Peristiwa besar yang baru saja terjadi: Gempa bumi berkekuatan 6,2 SR melanda daerah Sulawesi Tengah,menyebabkan kerusakan property dan beberapa korban jiwa. Kami berdoa untuk keselamatan semua yang terkena dampak dan berharap agar dapat segera ditangani. #gempabumi #bencanaalam #SulawesiTengah(created by:chatGPT )'''
+     txt_cf=st.text_area("""**Masukkan sembarang teks/paragraph**""")
      ,max_chars=200, height=150)
      
