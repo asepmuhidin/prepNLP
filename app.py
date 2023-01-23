752dd8033941bcca7de529d253110e8941e89c5d
Asep Muhidin <asep.muhidin@gmail.com>
2023-01-23 09:15:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
+st.set_page_config(
+     page_title="PrepNLP",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
+
 st.write("""
 # Preprosesing NLP
 Aplikasi pre-processing pada ***NLP (Natural Languange Processing)***
