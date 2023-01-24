a5d496ed2e3da26fe4ac5a4db69627e65de9a679
Asep Muhidin <asep.muhidin@gmail.com>
2023-01-24 10:52:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,28 +30,3 @@ st.write("""
 # NLP Preprocessing
 Aplikasi pre-processing pada ***NLP (Natural Languange Processing)***
 """)
-
-case_folding=st.checkbox("Case Folding")
-tokenizer=st.checkbox("Tokenizing")
-
-def CF(teks):
-     return teks.lower()
-
-if case_folding:
-     st.write("""### Case Folding""")
-     st.write("""
-     Case Folding adalah proses perubahan huruf kapital menjadi huruf kecil. 
-     Tujuannya yaitu kata-kata yang sama tidak terdeteksi berbeda hanya karena perbedaan terdapat huruf kapital.""")
-         
-     txt_cf=st.text_area("""**Masukkan sembarang teks/paragraph**""",max_chars=200, height=150)
-     if st.button('''***Case Folding***''',type='primary'):
-          code = '''def CF(teks):
-    return teks.lower()'''
-          st.code(code, language='python')
-          st.success("OUTPUT : " + CF(txt_cf))
-          
-elif tokenizer:
-  st.write("""### Tkenizing""")
-  st.write("""
-  Tokenizing merupakan proses memisahkan atau memecahkan yang awalnya berupa kalimat menjadi kata-kata atau memutus urutan string 
-  menjadi potongan-potongan seperti kata-kata berdasarkan tiap kata yang menyusunnya (Asiyah & Fithriasari, 2016).""")
