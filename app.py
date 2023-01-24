f3e9d23fc589b3bd3dce688ff27a64029f1f3f89
Asep Muhidin <asep.muhidin@gmail.com>
2023-01-24 13:36:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(
-     page_title="PrepNLP",
-     layout="centered"
- )
+def main():
+     st.write('#Welcome')
 
-st.markdown(
-    """
-    <style>
-        [data-testid=stSidebar] [data-testid=stImage]{
-            text-align: center;
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-            width: 100%;
-        }
-    </style>
-    """, unsafe_allow_html=True
-)
+if __function__='__main__':
+     main()
 
-image=Image.open("images/photoku.jpg")
-
-with st.sidebar:
-    st.image(image, width=120)
-    st.markdown("<h3 style='text-align: center; color: grey;'>Asep Muhidin  👋</h1>", unsafe_allow_html=True) 
-
-st.write("""
-# NLP Preprocessing
-Aplikasi pre-processing pada ***NLP (Natural Languange Processing)***
-""")
+     
